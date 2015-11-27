885e55fd3279628eb5a422c5803cc74a5ffd6083
unknown <ewh502@PC617S.its.york.ac.uk>
2015-11-27 16:40:30 +0000

    initial commit

---
+++ desktop/src/com/muscovy/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.muscovy.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.muscovy.game.MuscovyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MuscovyGame(), config);
+	}
+}
