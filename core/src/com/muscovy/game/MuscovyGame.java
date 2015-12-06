a68028704f40e36017e093d17749259bd750d291
SeldomBucket <Ed.Hathaway@hotmail.co.uk>
2015-12-06 00:34:08 +0000

    initial rendering working

--- core/src/com/muscovy/game/MuscovyGame.java
+++ core/src/com/muscovy/game/MuscovyGame.java
@@ -1,32 +1,129 @@
 package com.muscovy.game;
 
+		import com.badlogic.gdx.Input;
+		import com.badlogic.gdx.ApplicationListener;
+		import com.badlogic.gdx.InputProcessor;
 		import com.badlogic.gdx.ApplicationAdapter;
 		import com.badlogic.gdx.Gdx;
 		import com.badlogic.gdx.graphics.GL20;
+		import com.badlogic.gdx.graphics.OrthographicCamera;
 		import com.badlogic.gdx.graphics.Texture;
+		import com.badlogic.gdx.graphics.g2d.Sprite;
 		import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MuscovyGame extends ApplicationAdapter {
+public class MuscovyGame extends ApplicationAdapter implements ApplicationListener, InputProcessor {
+	private OrthographicCamera camera;
 	SpriteBatch batch;
 	Texture img;
+	RoomRenderer room;
+	Room drawRoom;
+	Enemy enemy1, enemy2;
+	PlayerCharacter playerCharacter;
+	Sprite roomSprite, testSprite1, testSprite2;
+	boolean keyflagW,keyflagD,keyflagA,keyflagS;
+	float w = 1280;
+	float h = 720;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("core/assets/badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, w, h);
+		roomSprite = new Sprite();
+		roomSprite.setTexture(new Texture("core/assets/Untitled.jpg"));
+		roomSprite.setX(0);
+		roomSprite.setY(0);
+		drawRoom = new Room();
+		drawRoom.setSprite(roomSprite);
+		playerCharacter = new PlayerCharacter();
+		testSprite2 = new Sprite();
+		testSprite2.setTexture(new Texture("core/assets/thing2.jpg"));
+		enemy2 = new Enemy();
+		enemy2.setSprite(testSprite2);
+		enemy2.setX(100);
+		enemy2.setY(100);
+		testSprite1 = new Sprite();
+		testSprite1.setTexture(new Texture("core/assets/thing.jpg"));
+		enemy1 = new Enemy();
+		enemy1.setSprite(testSprite2);
+		enemy1.setX(150);
+		enemy1.setY(150);
+		room = new RoomRenderer(drawRoom);
+		room.addNewDrawable(enemy1);
+		room.addNewDrawable(enemy2);
+		room.addNewDrawable(playerCharacter);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		playerMovement();
+        batch.setProjectionMatrix(camera.combined);
+		room.render(batch);
 	}
-}
 
-//Hello
-//here's another one
-/* Another comment */
-/* Michael's Awesome comment */
+	public void playerMovement(){
+		if(keyflagW) playerCharacter.Up();
+		if(keyflagA) playerCharacter.Right();
+		if(keyflagD) playerCharacter.Left();
+		if(keyflagS) playerCharacter.Down();
+		if(!keyflagD && !keyflagA){
+			playerCharacter.decelXToStop();
+		}
+		if(!keyflagW && !keyflagS){
+			playerCharacter.decelYToStop();
+		}
+		playerCharacter.movement();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		if(keycode == Input.Keys.W) keyflagW = true;
+		if(keycode == Input.Keys.S) keyflagS = true;
+		if(keycode == Input.Keys.D) keyflagD = true;
+		if(keycode == Input.Keys.A) keyflagA = true;
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.W) keyflagW = false;
+		if(keycode == Input.Keys.S) keyflagS = false;
+		if(keycode == Input.Keys.D) keyflagD = false;
+		if(keycode == Input.Keys.A) keyflagA = false;
+		return true;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
+	}
+
+
+}
--- core/src/com/muscovy/game/PlayerCharacter.java
+++ core/src/com/muscovy/game/PlayerCharacter.java
@@ -1,10 +1,151 @@
 package com.muscovy.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 /**
  * Created by ewh502 on 04/12/2015.
  */
 public class PlayerCharacter extends OnscreenDrawable{
+    private float xVelocity, yVelocity;
+    private float maxVelocity = 500, accel = 3000, decel = 2000;
+    float w = 1280;
+    float h = 720;
+    public PlayerCharacter(){
+        xVelocity = 0;
+        yVelocity = 0;
+        setX(0);
+        setY(0);
+        Sprite tempSprite;
+        tempSprite = new Sprite();
+        tempSprite.setBounds(800 / 2 - 64 / 2,20,64,64);
+        tempSprite.setTexture(new Texture(Gdx.files.internal("core/assets/duckplaceholder.jpg")));
+        setSprite(tempSprite);
+    }
+    public float getXVelocity(){
+        return xVelocity;
+    }
+    public void changeXVelocity(float velocity){
+        xVelocity += velocity;
+        if (xVelocity > maxVelocity) xVelocity = maxVelocity;
+        if (xVelocity < -maxVelocity) xVelocity = -maxVelocity;
+    }
+    public void setXVelocity(float velocity){
+        xVelocity = velocity;
+        if (xVelocity > maxVelocity) xVelocity = maxVelocity;
+        if (xVelocity < -maxVelocity) xVelocity = -maxVelocity;
+    }
+    public float getYVelocity(){
+        return yVelocity;
+    }
+    public void changeYVelocity(float velocity){
+        yVelocity += velocity;
+        if (yVelocity > maxVelocity) yVelocity = maxVelocity;
+        if (yVelocity < -maxVelocity) yVelocity = -maxVelocity;
+    }
+    public void setYVelocity(float velocity){
+        yVelocity = velocity;
+        if (yVelocity > maxVelocity) yVelocity = maxVelocity;
+        if (yVelocity < -maxVelocity) yVelocity = -maxVelocity;
+    }
+
+    public void Left() {
+        if (Gdx.input.isKeyPressed(Input.Keys.D) && Gdx.input.isKeyPressed(Input.Keys.A)) {
+            decelXToStop();
+        } else {
+            changeXVelocity(accel*Gdx.graphics.getDeltaTime());
+            checkEdgeCollision();
+        }
+    }
+    public void Right(){
+        changeXVelocity((-accel)*Gdx.graphics.getDeltaTime());
+        checkEdgeCollision();
+    }
+    public void Up() {
+        if (Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.S)) {
+            decelYToStop();
+        } else {
+            changeYVelocity(accel * Gdx.graphics.getDeltaTime());
+            checkEdgeCollision();
+        }
+    }
+    public void Down() {
+        changeYVelocity((-accel) * Gdx.graphics.getDeltaTime());
+        checkEdgeCollision();
+    }
+    public void movement(){
+        setX(getX() + xVelocity*Gdx.graphics.getDeltaTime());
+        setY(getY() + yVelocity*Gdx.graphics.getDeltaTime());
+    }
+    private void checkEdgeCollision(){
+        if(getX() < 0) {setX(0); setXVelocity(0);}
+        if(getX() > w - 64) {setX(w-64); setXVelocity(0);}
+        if(getY() < 0) {setY(0); setYVelocity(0);}
+        if(getY() > h - 64) {setY(h-64); setYVelocity(0);}
+    }
+    public void decelXToStop(){
+        if (getXVelocity() > 0){
+            if (getXVelocity() - decel*Gdx.graphics.getDeltaTime() < 0){
+                setXVelocity(0);
+            }else{
+                changeXVelocity(-decel*Gdx.graphics.getDeltaTime());
+            }
+        }
+        if (getXVelocity() < 0){
+            if (getXVelocity() + decel*Gdx.graphics.getDeltaTime() > 0){
+                setXVelocity(0);
+            }else{
+                changeXVelocity(decel*Gdx.graphics.getDeltaTime());
+            }
+        }
+    }
+    public void decelYToStop(){
+        if (getYVelocity() > 0){
+            if (getYVelocity() - decel*Gdx.graphics.getDeltaTime() < 0){
+                setYVelocity(0);
+            }else{
+                changeYVelocity(-decel*Gdx.graphics.getDeltaTime());
+            }
+        }
+        if (getYVelocity() < 0){
+            if (getYVelocity() + decel*Gdx.graphics.getDeltaTime() > 0){
+                setYVelocity(0);
+            }else{
+                changeYVelocity(decel*Gdx.graphics.getDeltaTime());
+            }
+        }
+    }
+
+    @Override
+    public Sprite getSprite() {
+        return super.getSprite();
+    }
+
+    @Override
+    public void setSprite(Sprite sprite) {
+        super.setSprite(sprite);
+    }
+
+    @Override
+    public float getX() {
+        return super.getX();
+    }
+
+    @Override
+    public void setX(float x) {
+        super.setX(x);
+    }
+
+    @Override
+    public float getY() {
+        return super.getY();
+    }
+
+    @Override
+    public void setY(float y) {
+        super.setY(y);
+    }
 
 }
