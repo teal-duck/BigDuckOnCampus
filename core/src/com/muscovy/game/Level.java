f08a23c5002cac16babc25464c3ec9f76eaba37f
unknown <ewh502@PC637S.its.york.ac.uk>
2016-01-11 12:44:44 +0000

    added level class for encapsulation

---
+++ core/src/com/muscovy/game/Level.java
@@ -0,0 +1,12 @@
+package com.muscovy.game;
+
+/**
+ * Created by ewh502 on 11/01/2016.
+ */
+public class Level {
+    Room[][] levelArray;
+
+    public Level(Room[][] levelArray) {
+        this.levelArray = levelArray;
+    }
+}
