ff38535db9d7b13bd3a4d75db34401697a845085
aacn500 <aacn500@users.noreply.github.com>
2016-02-09 11:20:00 +0000

    better method to get item sprites

--- core/src/com/muscovy/game/enums/ItemType.java
+++ core/src/com/muscovy/game/enums/ItemType.java
@@ -1,6 +1,20 @@
 package com.muscovy.game.enums;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.muscovy.game.AssetLocations;
+import com.muscovy.game.MuscovyGame;
 
 public enum ItemType {
 	HEALTH, COIN, TRIPLE_SHOT;
+	
+	public static Texture getItemSprite(MuscovyGame game, ItemType itemType) {
+		switch (itemType) {
+		case HEALTH:
+			return game.getTextureMap().getTextureOrLoadFile(AssetLocations.HEALTHPACK);
+		case TRIPLE_SHOT:
+			return game.getTextureMap().getTextureOrLoadFile(AssetLocations.TRIPLE_SHOT);
+		default:
+			return null;
+		}
+	}
 }
--- core/src/com/muscovy/game/level/DungeonRoom.java
+++ core/src/com/muscovy/game/level/DungeonRoom.java
@@ -256,8 +256,10 @@ public class DungeonRoom {
 			enemy.setXTiles(DungeonRoom.FLOOR_WIDTH_IN_TILES - 1);
 			enemy.setYTiles(DungeonRoom.FLOOR_HEIGHT_IN_TILES - 1);
 			addEnemy(enemy);
-			Sprite itemSprite = new Sprite(game.getTextureMap().getTextureOrLoadFile(AssetLocations.TRIPLE_SHOT));
-			Item item = new Item(game, itemSprite, new Vector2(0, 0), ItemType.TRIPLE_SHOT);
+			
+			ItemType itemType = ItemType.TRIPLE_SHOT;
+			Sprite itemSprite = new Sprite(ItemType.getItemSprite(game, itemType));
+			Item item = new Item(game, itemSprite, new Vector2(0, 0), itemType);
 			item.setXTiles(5);
 			item.setYTiles(5);
 			addItem(item);
